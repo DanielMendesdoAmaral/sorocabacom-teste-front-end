c535bac98186bbdbaa5fc41f695311d33990e1e5
DanielMendesdoAmaral <daniel.amaral720@gmail.com>
2021-01-19 01:18:18 -0300

    Banner 90% pronto

--- Transistor/src/pages/landingpage/landingpage.js
+++ Transistor/src/pages/landingpage/landingpage.js
@@ -7,7 +7,11 @@ import "./landingpage.css";
 import Card from "../../components/cards/card";
 
 //Assets
-import Logo from "../../assets/Logo/Logo.png"
+import Logo from "../../assets/Logo/Logo.png";
+import RedCard1 from "../../assets/Card 1/Imagem card-1.png";
+import PenaCaidaEsquerda from "../../assets/Card1-Details/Pena-1.png";
+import PenaCaidaDireita from "../../assets/Card1-Details/Pena-2.png";
+import PenaCaidaReta from "../../assets/Card1-Details/Pena-4.png";
 
 const LandingPage = () => {
     return (
@@ -16,6 +20,15 @@ const LandingPage = () => {
                 <img src={Logo} alt="SuperGiantGames" title="SuperGiantGames"></img>
                 <h1 className="montserrat texto-branco">SuperGiantGames</h1>
             </header>
+            <main>
+                <section className="banner">
+                    <div className="container-img-card1">
+                        <h1 className="maiusculo texto-branco bold">Transistor - Red the singer</h1>
+                        <img src={RedCard1} alt="Red" title="Red"></img>
+                        <p className="">"Olha, o que quer que você<br></br>esteja pensando, me faça<br></br>um favor, não solte."</p>
+                    </div>
+                </section>
+            </main>
         </>
     );
 }
