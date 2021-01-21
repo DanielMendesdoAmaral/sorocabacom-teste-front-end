52e7b467f9da8461886bb861c82239f6876359b4
DanielMendesdoAmaral <daniel.amaral720@gmail.com>
2021-01-20 21:42:07 -0300

    Updates

---
+++ precache-manifest.65b04f284dce13cac0da50567ac17e0b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3fbbcc27469f2313d75f17f53914f8",
+    "url": "/sorocabacom-teste-front-end/index.html"
+  },
+  {
+    "revision": "c1895af6e1574b5da05d",
+    "url": "/sorocabacom-teste-front-end/static/css/2.fc9b5189.chunk.css"
+  },
+  {
+    "revision": "c8ddf60bb193273440b3",
+    "url": "/sorocabacom-teste-front-end/static/css/main.cb8d491f.chunk.css"
+  },
+  {
+    "revision": "c1895af6e1574b5da05d",
+    "url": "/sorocabacom-teste-front-end/static/js/2.e420b52a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/sorocabacom-teste-front-end/static/js/2.e420b52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ddf60bb193273440b3",
+    "url": "/sorocabacom-teste-front-end/static/js/main.1aa727aa.chunk.js"
+  },
+  {
+    "revision": "235ccee518e6286ce33d",
+    "url": "/sorocabacom-teste-front-end/static/js/runtime-main.4a15c81f.js"
+  },
+  {
+    "revision": "b3c576fa9512bcd46040b8a246c24695",
+    "url": "/sorocabacom-teste-front-end/static/media/Background.b3c576fa.png"
+  },
+  {
+    "revision": "c1686559dad8805cf1fb1cea37faa0e5",
+    "url": "/sorocabacom-teste-front-end/static/media/Background.c1686559.svg"
+  },
+  {
+    "revision": "95b92b9e4ad36a7b713ef64298f8850d",
+    "url": "/sorocabacom-teste-front-end/static/media/Grant.95b92b9e.png"
+  },
+  {
+    "revision": "66f015b3b8537052574061ba3e931002",
+    "url": "/sorocabacom-teste-front-end/static/media/Imagem card-1.66f015b3.png"
+  },
+  {
+    "revision": "17d234768f7d75e9bc19051580c71e96",
+    "url": "/sorocabacom-teste-front-end/static/media/Red.17d23476.png"
+  },
+  {
+    "revision": "31cda35a77c154d13c7f96037f72b0ea",
+    "url": "/sorocabacom-teste-front-end/static/media/Sybil_2.31cda35a.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/sorocabacom-teste-front-end/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/sorocabacom-teste-front-end/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/sorocabacom-teste-front-end/static/media/revicons.2feb69cc.eot"
+  }
+]);
